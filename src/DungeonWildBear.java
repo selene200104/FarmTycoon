d89b309abb8f236aff5af8154775bd2fe1e95620
Juhee Jang <selene170402@naver.com>
2020-03-31 16:21:18 +0900

    Add Dungeon Item

--- src/DungeonWildBear.java
+++ src/DungeonWildBear.java
@@ -30,9 +30,7 @@ public class DungeonWildBear extends JPanel {
 	static int randomMushroom;	
 	static int randomItem;	//랜덤으로 받을 수 있는 아이템의 번호
 	static int numOfRun;	//도망칠 수 있는 랜덤 번호
-	static int numOfRandomMushroom = 2;
-	static int numOfBone = 1;
-	static int money;
+
 	//static int wildBearHp = 110;
 
 	static Random randomMushroomSelect = new Random();
@@ -61,25 +59,32 @@ public class DungeonWildBear extends JPanel {
 		setBounds(0, 0, 800, 600);
 		setBackground(Color.black);
 		
-		WildAnimal wildBear = new WildAnimal("야생곰", 100, 10, true);
+		WildAnimal wildBear = new WildAnimal("야생곰", 100, 15, true);
+		
 		
 		JLabel wildBearImage = new JLabel();
 		wildBearImage.setHorizontalAlignment(SwingConstants.CENTER);
 		wildBearImage.setIcon(new ImageIcon("./images/wildbear.png"));
-		wildBearImage.setBounds(263, -11, 266, 216);
+		wildBearImage.setBounds(292, 10, 224, 170);
 		add(wildBearImage);
 
-		JTextArea textArea = new JTextArea();
-		textArea.setBounds(68, 218, 662, 216);
-		textArea.setEnabled(true);
-		textArea.setFont((new Font("굴림체", Font.BOLD, 15)));
-
-		JScrollPane scrollBar = new JScrollPane(textArea);
-		scrollBar.setBounds(68, 218, 662, 216);
+		JScrollPane scrollBar = new JScrollPane();
+		scrollBar.setBounds(68, 201, 662, 186);
 		scrollBar.getVerticalScrollBar().setValue(scrollBar.getVerticalScrollBar().getMaximum());
 		add(scrollBar);
 		
-		// 야생공 자동 동격 쓰레드
+		JTextArea textArea = new JTextArea();
+		scrollBar.setViewportView(textArea);
+		textArea.setEnabled(true);
+		textArea.setFont((new Font("굴림체", Font.BOLD, 15)));
+		
+		JPanel itemPanel = new JPanel();
+		itemPanel.setBounds(273, 397, 252, 58);
+		itemPanel.setLayout(null);
+		itemPanel.setVisible(false);
+		add(itemPanel);
+		
+		// 야생곰 자동 동격 쓰레드
 		new Thread(new Runnable() {
 			public void run() {
 				try {
@@ -101,7 +106,7 @@ public class DungeonWildBear extends JPanel {
 					scrollBar.getVerticalScrollBar().setValue(scrollBar.getVerticalScrollBar().getMaximum());
 
 					// 게임에 지면 다음날로 바뀌고, 플레이어의 체력이 50으로 시작한다.
-					if (Player2.hp == 0) {
+					if (Player2.hp <= 0) {
 						Main.day ++;
 						Main.energy = 50;
 						//System.out.println("오늘은 " + Main.day + "일입니다. 플레이어 체력은 " + Main.energy + "입니다.");
@@ -116,6 +121,8 @@ public class DungeonWildBear extends JPanel {
 							setVisible(false);
 							//interrupt();
 							Main.btnNewButton.setVisible(true);
+							JOptionPane.showMessageDialog(null, "야생곰한테 당하고 말았습니다.", "SYSTEM", JOptionPane.INFORMATION_MESSAGE);
+
 							//Main.dungeonWildBear.setVisible(true);
 							// dungeonEx.setVisible(true);
 						//} catch (Exception ie) {
@@ -149,7 +156,7 @@ public class DungeonWildBear extends JPanel {
 							// System.out.println("돈 받기 전 소유머니: " +money);
 							if (randomItem == 0) {
 								//textArea.append("SYSTEM: 뼛가루를 받았습니다! \n");
-								numOfBone++;
+								Player2.numOfBone++;
 
 								try {
 									Thread.sleep(1000);
@@ -166,7 +173,7 @@ public class DungeonWildBear extends JPanel {
 								//System.out.println("뼛가루 받은 후 갯수: " +numOfBone);
 							} else if (randomItem == 1) {
 								//textArea.append("SYSTEM: 랜덤버섯을 받았습니다! \n");
-								numOfRandomMushroom++;
+								Player2.numOfRandomMushroom++;
 								
 								try {
 									Thread.sleep(1000);
@@ -177,14 +184,14 @@ public class DungeonWildBear extends JPanel {
 									ie.printStackTrace();
 								}
 								
-								System.out.println("복불복버섯 받은 후 갯수: " +numOfRandomMushroom);
+								System.out.println("복불복버섯 받은 후 갯수: " +Player2.numOfRandomMushroom);
 								JOptionPane.showMessageDialog(null, "복불복버섯을 받았습니다", "SYSTEM", JOptionPane.INFORMATION_MESSAGE);
 
 								Main.btnNewButton.setVisible(true);
 							} else if (randomItem == 2) {
 								//textArea.append("SYSTEM: 돈을 받았습니다! \n");
-								money = money + 1000;
-								System.out.println("돈 받은 후 소유 머니: " +money);
+								Player2.money = Player2.money + 2000;
+								System.out.println("돈 받은 후 소유 머니: " +Player2.money);
 								
 								try {
 									Thread.sleep(1000);
@@ -205,19 +212,26 @@ public class DungeonWildBear extends JPanel {
 				}
 			}
 		});
-		attackButton.setBounds(68, 458, 168, 47);
+		attackButton.setBounds(68, 465, 168, 47);
 		add(attackButton);
-
+		
 		useItemButton = new JButton("아이템 사용");
 		useItemButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-
-				System.out.println("복불복버섯 사용 전 갯수: " + numOfRandomMushroom);
+				itemPanel.setVisible(true);
+			}
+		});
+		
+		JButton useRandomMushroomButton = new JButton("복불복버섯");
+		useRandomMushroomButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				System.out.println("복불복버섯 사용 전 갯수: " + Player2.numOfRandomMushroom);
 
 				if (Player2.hp > 0) {
 					if (wildBear.hp > 0) {
-						if (numOfRandomMushroom > 0) {
-							numOfRandomMushroom--;
+						if (Player2.numOfRandomMushroom > 0) {
+							Player2.numOfRandomMushroom--;
 							// System.out.println("복불복버섯 사용 후 남은 갯수: " +numOfRandomMushroom);
 							randomMushroomSelect();
 							// System.out.println("복불복버섯 선택번호: "randomMushroom);
@@ -228,6 +242,7 @@ public class DungeonWildBear extends JPanel {
 									Player2.hp = 100;
 								}
 								textArea.append("복불복버섯의 효과로 플레이어의 피 증가! 체력:" + Player2.hp + "\n");
+								itemPanel.setVisible(false);
 							}
 
 							if (randomMushroom == 1) {
@@ -257,9 +272,60 @@ public class DungeonWildBear extends JPanel {
 					}
 				}
 			}
-
 		});
-		useItemButton.setBounds(317, 458, 168, 47);
+		//btnNewButton.setFont(new Font("굴림체", Font.PLAIN, 7));
+		useRandomMushroomButton.setBounds(12, 10, 68, 37);
+		itemPanel.add(useRandomMushroomButton);
+		
+		JButton usePotionHpOf30_Button = new JButton("체력 30% 포션");
+		usePotionHpOf30_Button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (Player2.hp > 0) {
+					if (wildBear.hp > 0) {
+						if(Player2.numOfPotionHp_30 >0) {
+							Player2.numOfPotionHp_30--;
+							Player2.hp = Player2.hp +30;
+							if(Player2.hp > 100) {
+								Player2.hp = 100;
+							}
+							textArea.append("체력 30% 추가 포션을 사용했습니다!. 체력 UP+30! \n");
+							itemPanel.setVisible(false);
+						}else {
+							textArea.append("체력 30% 추가 포션이 없어서 사용할 수 없습니다. \n");
+							itemPanel.setVisible(false);
+						}
+					}
+				}
+			}
+		});
+		usePotionHpOf30_Button.setBounds(92, 10, 68, 37);
+		itemPanel.add(usePotionHpOf30_Button);
+		
+		JButton usePotionHpOf50_Button = new JButton("체력 50% 포션");
+		usePotionHpOf50_Button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (Player2.hp > 0) {
+					if (wildBear.hp > 0) {
+						if(Player2.numOfPotionHp_50 >0) {
+							Player2.numOfPotionHp_50--;
+							Player2.hp = Player2.hp +50;
+							if(Player2.hp > 100) {
+								Player2.hp = 100;
+							}
+							textArea.append("체력 50% 추가 포션을 사용했습니다!. 체력 UP+50! \n");
+							itemPanel.setVisible(false);
+						}else {
+							textArea.append("체력 50% 추가 포션이 없어서 사용할 수 없습니다. \n");
+							itemPanel.setVisible(false);
+						}
+					}
+				}
+			}
+		});
+		usePotionHpOf50_Button.setBounds(172, 10, 68, 37);
+		itemPanel.add(usePotionHpOf50_Button);
+		
+		useItemButton.setBounds(317, 465, 168, 47);
 		add(useItemButton);
 
 		runAwayButton = new JButton("도망치기");
@@ -289,8 +355,8 @@ public class DungeonWildBear extends JPanel {
 			}
 		});
 		
-		runAwayButton.setBounds(562, 458, 168, 47);
+		runAwayButton.setBounds(562, 465, 168, 47);
 		add(runAwayButton);
+		
 	}
-
 }
--- src/Player2.java
+++ src/Player2.java
@@ -6,7 +6,11 @@ public class Player2 {
 		static Random randomPower = new Random();
 		static int hp = 100;
 		static int attackPower; 
-
+		static int numOfBone = 1;
+		static int numOfRandomMushroom = 2;
+		static int numOfPotionHp_30 = 3;
+		static int numOfPotionHp_50 = 3;
+		static int money;
 		/*static void playerAttack(){
 			attackPower = randomPower.nextInt(11) + 10; //플레이어는 10~20의 공격 파워로 공격을 함
 			
